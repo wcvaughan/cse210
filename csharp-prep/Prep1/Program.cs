2f7dd01b8ffcc9067d92fe4dd8bc1436a43fe919
wcvaughan <chrisvaughanlds@gmail.com>
2024-06-02 23:30:06 -0800

    Completed prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        /*Prompt the user for their first name. Then, prompt them for their last name. 
+        Display the text back all on one line saying, "Your name is last-name, first-name, last-name"*/
+
+        Console.WriteLine("What is your first name?");
+        string f_name = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string l_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}");
+        Console.ReadLine();
     }
 }
